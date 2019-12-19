54003a26565dce509bc2563b62761f8146d9cfdb
jod517 <58477142+jod517@users.noreply.github.com>
2019-12-19 15:41:43 +0300

    Init commit

--- src/main/java/servlets/TestServlet.java
+++ src/main/java/servlets/TestServlet.java
@@ -11,15 +11,17 @@ import java.io.PrintWriter;
 @WebServlet("/mult")
 public class TestServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, NumberFormatException {
         String value = req.getParameter("value");
         PrintWriter writer = resp.getWriter();
-        try {
+        if (value == null || value.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            System.out.println(0);
+        } else {
             writer.println(Integer.parseInt(value) * 2);
-            resp.setStatus(200);
-        } catch (NumberFormatException ex){
-            resp.setStatus(400);
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+
         }
 
     }
-}
